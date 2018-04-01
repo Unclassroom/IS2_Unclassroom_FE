c2a2eb269ec36fa3fe0d4deac982a3eb562951e5
Ramle <ramle_12@hotmail.com>
2018-04-01 09:47:00 -0500

    fix

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Angular2TokenService} from "angular2-token";
-import {environment} from "../environments/environment";
+import {Angular2TokenService} from 'angular2-token';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent 
-//implements OnInit 
+export class AppComponent
+//implements OnInit
 {
   // subjects = [];
   // constructor(private http:HttpClient) {
@@ -33,7 +33,7 @@ export class AppComponent
 
         console.log('auth response:', res);
         console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
-        console.log('auth response body:', res.json()); //log the response body to show the user 
+        console.log('auth response body:', res.json()); //log the response body to show the user
       },
 
       err => {
