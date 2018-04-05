5ba120829817565a9fbd4e0b8a230bd7bcd7e5d6
Ramle <ramle_12@hotmail.com>
2018-04-05 09:41:58 -0500

    Branch data

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { Angular2TokenService } from 'angular2-token';
 import { HttpModule } from '@angular/http';
+import { UsuariosService } from './usuarios.service';
 
 
 @NgModule({
@@ -31,7 +32,8 @@ import { HttpModule } from '@angular/http';
   ],
   providers: [
     Angular2TokenService,
-    HttpClientModule],
+    HttpClientModule,
+    UsuariosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, OnInit, Output, EventEmitter, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import { Router } from '@angular/router';
-import {Angular2TokenService} from "angular2-token";
+import {Angular2TokenService} from 'angular2-token';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent implements OnInit {
   signInUser = {
     email: '',
     password: ''
   };
+
 
   @Output() onFormResult = new EventEmitter<any>();
   // constructor(public router: Router) {}
@@ -24,17 +29,16 @@ export class LoginComponent implements OnInit {
   //  }
   constructor(private authToken: Angular2TokenService) { }
   ngOnInit() {}
-  onSignInSubmit()
-  {
+  onSignInSubmit() {
     this.authToken.signIn(this.signInUser).subscribe(
         res => {
           console.log('auth response:', res);
-          console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
-           console.log('auth response body:', res.json()); //log the response body to show the user 
-          if(res.status == 200){
+          console.log('auth response headers: ', res.headers.toJSON()); // log the response header to show the auth token
+           console.log('auth response body:', res.json()); // log the response body to show the user
+          if ( res.status === 200 ) {
             console.log('auth response:', res);
-            console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
-            console.log('auth response body:', res.json()); //log the response body to show the user 
+            console.log('auth response headers: ', res.headers.toJSON()); // log the response header to show the auth token
+            console.log('auth response body:', res.json()); // log the response body to show the user
             this.onFormResult.emit({signedIn: true, res});
           }
         },
@@ -43,7 +47,7 @@ export class LoginComponent implements OnInit {
           console.log('err:', err);
           this.onFormResult.emit({signedIn: false, err});
         }
-    )
+    );
   }
   // onLoggedin() {
   //   // ngIf="tokenAuthService.userSignedIn()"
