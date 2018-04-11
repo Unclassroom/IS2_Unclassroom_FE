cdff3445a99ff9f24cac5b7cb5891cf8ddf5d722
Jonathan Campo <joacampora@unal.edu.co>
2018-04-11 06:39:55 -0500

    Fin IT 2

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {LandingComponent} from "./landing/landing.component";
 
 const routes: Routes = [
   { path: 'home', loadChildren: './landing/landing.module#LandingModule'},
+  { path: 'landing', loadChildren: './landing/landing.module#LandingModule'},
   { path: '', loadChildren:'./landing/landing.module#LandingModule', pathMatch: 'full'},
   { path: 'layout', loadChildren: './layout/layout.module#LayoutModule'},
   { path: 'login', loadChildren: './login/login.module#LoginModule' },
