2f7022683e7efd30282ed226e3ef52ef0947220f
Ramle <ramle_12@hotmail.com>
2018-04-11 22:12:12 -0500

    Last Changes

--- Frontend/src/app/usuarios.service.ts
+++ Frontend/src/app/usuarios.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {Subject, Observable} from "rxjs";
+import {Subject, Observable} from 'rxjs';
 import { of } from 'rxjs/observable/of';
 import { catchError, map, tap } from 'rxjs/operators';
 import {HttpClient, HttpHeaders } from '@angular/common/http';
-import {Angular2TokenService} from "angular2-token";
+import {Angular2TokenService} from 'angular2-token';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -12,12 +12,12 @@ const httpOptions = {
 @Injectable()
 export class UsuariosService {
 
-  userSignedIn$:Subject<boolean> = new Subject();
+  userSignedIn$: Subject<boolean> = new Subject();
   constructor(
-    public usuariosService:Angular2TokenService) {
+    public usuariosService: Angular2TokenService) {
 
     this.usuariosService.validateToken().subscribe(
-      res => res.status == 200 ? this.userSignedIn$.next(res.json().success) : this.userSignedIn$.next(false)
+      res => res.status === 200 ? this.userSignedIn$.next(res.json().success) : this.userSignedIn$.next(false)
     )
   }
 
@@ -25,11 +25,11 @@ export class UsuariosService {
 
   /** POST: add a new hero to the server */
 
-  registerUser(signUpData:  {email:string, password:string, passwordConfirmation:string}):Observable<Response>{
+  registerUser(signUpData:  {email: string, password: string, passwordConfirmation: string}): Observable<Response>{
     return this.usuariosService.registerAccount(signUpData).map(
       res => {
         this.userSignedIn$.next(true);
-        return res
+        return new Response
       }
     );
   }
