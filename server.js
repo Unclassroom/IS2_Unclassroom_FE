5587514ec76359c2306f4aaf66a3c7378f6cd654
Luis Felipe Epia Realpe <lfepiar@unal.edu.co>
2018-04-15 20:21:57 -0500

    again heroku

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 //Install express server
 const express = require('express');
 const app = express();
+var port = process.env.PORT || 8000
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+   // configure stuff here
+    app.use(express.static(__dirname + '/'));   
+} else {
+    app.use(express.static(__dirname + '/'));
+}
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
