7820d409169e97208aca1e47fa8179821c4bbd9c
Luis Felipe Epia Realpe <lfepiar@unal.edu.co>
2018-04-15 20:51:25 -0500

    new tutorial

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 //Install express server
 const express = require('express');
 const app = express();
-var port = process.env.PORT || 8000
 
-// Serve only the static files form the dist directory
-var env = process.env.NODE_ENV || 'development';
-if ('development' == env) {
-   // configure stuff here
-    app.use(express.static(__dirname + '/'));   
-} else {
-    app.use(express.static(__dirname + '/'));
-}
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist'));
+
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, rest) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+
+})
+
+console.log('Console listening!');
