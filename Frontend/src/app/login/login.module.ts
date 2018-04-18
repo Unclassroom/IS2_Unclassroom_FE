6002e7c3f6bc7d4966f787b3e5f6478ab131b39d
Jonathan Campo <joacampora@unal.edu.co>
2018-04-18 06:57:37 -0500

    Trying to do Redux

--- Frontend/src/app/login/login.module.ts
+++ Frontend/src/app/login/login.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { login } from '../redux/session';
+
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
--- Frontend/src/app/redux/store.ts
+++ Frontend/src/app/redux/store.ts
@@ -1,35 +1,7 @@
-import { ISession } from './session';
-import { ADD_SESSION, ADD_REGISTER, REMOVE_SESSION, REMOVE_REGISTER } from './actions';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import reducer from './session';
 
-export interface AppState {
-    session: ISession;
-    isLogged: boolean;
-    tempID: number;
-}
-
-export const INITIAL_STATE: AppState = {
-  session: {
-    token: undefined,
-    name: undefined,
-    role: undefined
-  },
-  isLogged: false,
-  tempID: undefined
-}
-
-export function rootReducer(state, action) {
-  switch (action.type) {
-    case ADD_SESSION:
-      let newState = Object.assign({}, state, { session: action.session, isLogged: true });
-      if (!action.session.token)
-        newState.session.token = state.session.token;
-      return newState;
-    case REMOVE_SESSION:
-      return Object.assign({}, state, INITIAL_STATE);
-    case ADD_REGISTER:
-      return Object.assign({}, state, { session: action.session });
-    case REMOVE_REGISTER:
-      return Object.assign({}, state, INITIAL_STATE);
-  }
-  return state;
-}
+const store = createStore(reducer, {}, applyMiddleware(thunk, logger));
+export default store;
