f48e63814fab346ef376a251e3cddefc8bd22a36
gadrielag <adorados@unal.edu.co>
2018-04-19 09:02:34 -0500

    comment store redux

--- Frontend/src/app/redux/store.ts
+++ Frontend/src/app/redux/store.ts
@@ -1,11 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 // import thunk from 'redux-thunk';
 // import logger from 'redux-logger';
-import reducer from './session';
+// import reducer from './session';
 
 // const store = createStore(reducer, {}, applyMiddleware(thunk, logger));
-<<<<<<< HEAD
-export default store;
-=======
 // export default store;
->>>>>>> social
